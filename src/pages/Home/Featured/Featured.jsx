6c610e0e8632aaeac6ae89270c9f6f1002ef0213
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-02 21:23:43 +0600

    added parallax in featured section, and added testimonials section

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,12 +5,12 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white pt-10 my-20'>
+        <div className='featured-item bg-fixed text-white pt-10 my-20'>
             <SectionTitle
                 subHeading={'Check It Out'}
                 heading={'Featured Item'}
             ></SectionTitle>
-            <div className='md:flex justify-center items-center pb-20 pt-12 px-36 gap-10'>
+            <div className='md:flex justify-center items-center bg-slate-500 bg-opacity-50 pb-20 pt-12 px-36 gap-10'>
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -18,7 +18,7 @@ const Featured = () => {
                     <p>June 01, 2025</p>
                     <p className='uppercase'>Where can i get some?</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae perferendis quia rem quae quis quasi nam modi ratione impedit maiores ex voluptates nulla dolores voluptas, facilis cumque explicabo animi nobis necessitatibus alias suscipit. Praesentium ratione, deserunt nulla tempore dicta sed, blanditiis iste nam ipsum distinctio eos omnis odit alias veritatis.</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4">Order Now</button>
                 </div>
             </div>
         </div>
