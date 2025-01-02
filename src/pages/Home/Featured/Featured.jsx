7608075a27e55b65083be40ecc9a6e320374ba16
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-02 20:55:47 +0600

    FeatureItem section added

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import featuredImg from '../../../assets/home/featured.jpg'
+import './Featured.css'
+
+const Featured = () => {
+    return (
+        <div className='featured-item text-white pt-10 my-20'>
+            <SectionTitle
+                subHeading={'Check It Out'}
+                heading={'Featured Item'}
+            ></SectionTitle>
+            <div className='md:flex justify-center items-center pb-20 pt-12 px-36 gap-10'>
+                <div>
+                    <img src={featuredImg} alt="" />
+                </div>
+                <div>
+                    <p>June 01, 2025</p>
+                    <p className='uppercase'>Where can i get some?</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae perferendis quia rem quae quis quasi nam modi ratione impedit maiores ex voluptates nulla dolores voluptas, facilis cumque explicabo animi nobis necessitatibus alias suscipit. Praesentium ratione, deserunt nulla tempore dicta sed, blanditiis iste nam ipsum distinctio eos omnis odit alias veritatis.</p>
+                    <button className="btn btn-outline">Order Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
