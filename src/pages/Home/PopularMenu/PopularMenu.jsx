94dddbb554b0dd239521dc5f45870014314c1192
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-02 18:56:29 +0600

    Popular Section added

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
+
+const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const popularItems = data.filter(item => item.category === 'popular');
+                setMenu(popularItems);
+            })
+    }, [])
+
+    return (
+        <div className='my-10'>
+            <SectionTitle
+                subHeading={'Popular Items'}
+                heading={'From Our Menu'}
+            ></SectionTitle>
+            <div className='grid md:grid-cols-2 gap-7'>
+                {
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularMenu;
