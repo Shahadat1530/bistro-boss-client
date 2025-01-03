eb6066efe971d31ec229d24060b0df5d9de1bb17
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-03 19:06:43 +0600

    used dynamic title

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -28,6 +28,9 @@ const PopularMenu = () => {
                     ></MenuItem>)
                 }
             </div>
+            <div className='text-center my-7'>
+                <button className="btn btn-outline border-0 border-b-4">View Full Menu</button>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const links = <>
-        <li><a>Item 1</a></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/menu'>Our Menu</Link></li>
     </>
     return (
         <div>
