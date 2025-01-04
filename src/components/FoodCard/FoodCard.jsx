d6f7afb30a479e55c09c53d9a3c999567c915fb8
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-04 15:21:36 +0600

    now the data coming from db

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const FoodCard = ({item}) => {
+const FoodCard = ({ item }) => {
     const { name, image, price, recipe } = item;
     return (
         <div className="card bg-base-100 w-96 shadow-xl">
@@ -13,8 +13,8 @@ const FoodCard = ({item}) => {
             <div className="card-body text-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-outline border-0 border-b-4 bg-slate-100 border-orange-400 hover:text-orange-400">Add to Cart</button>
                 </div>
             </div>
         </div>
