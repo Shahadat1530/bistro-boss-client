f64a0f41c03c3c1c83c324737d5ee81bb5bfed54
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-07 00:13:23 +0600

    used tanStack-Query for fetching the store data, also created custom hook for that.. and showing the cart length in navbar cartBar

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import useAuth from '../../hooks/useAuth';
 import Swal from 'sweetalert2';
 import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe, _id } = item;
     const { user } = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
+    const axiosSecure = useAxiosSecure();
 
     const handleAddToCart = (food) => {
         if (user && user?.email) {
@@ -20,7 +21,7 @@ const FoodCard = ({ item }) => {
                 price
             };
 
-            axios.post('http://localhost:5000/carts', cartItem)
+            axiosSecure.post('/carts', cartItem)
                 .then(res => {
                     if (res.data.insertedId) {
                         Swal.fire({
