137b03e65e6ef097596fc15fae1e85d6267be128
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-08 22:19:16 +0600

    storing the data in DB

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { FaGoogle } from 'react-icons/fa';
 import useAuth from '../../hooks/useAuth';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
     const { googleSignIn } = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
 
     const handleGoogle = () => {
         googleSignIn()
         .then(result => {
             console.log(result.user);
+            const userInfo = {
+                name: result.user?.displayName,
+                email: result.user?.email
+            };
+
+            axiosPublic.post('/users', userInfo)
+            .then(res => {
+                console.log(res.data);
+                navigate('/')
+            })
         })
     }
 
