da4d2e0fd286f7d3007b98be805221b0ad9a29c3
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-10 16:09:12 +0600

    can make admin now

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -15,7 +15,19 @@ const AllUsers = () => {
     });
 
     const handleMakeAdmin = user => {
-        
+        axiosSecure.patch(`/users/admin/${user._id}`)
+            .then(res => {
+                if (res.data.modifiedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${user.name} is an Admin now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
     }
 
     const handleDeleteUser = user => {
@@ -71,9 +83,12 @@ const AllUsers = () => {
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
                                     <td>
-                                        <button onClick={() => handleMakeAdmin(user)} className="btn bg-orange-500 text-xl btn-lg">
-                                            <FaUsers className='text-white'></FaUsers>
-                                        </button>
+                                        {
+                                            user.role === 'admin' ? 'Admin' :
+                                                <button onClick={() => handleMakeAdmin(user)} className="btn bg-orange-500 text-xl btn-lg">
+                                                    <FaUsers className='text-white'></FaUsers>
+                                                </button>
+                                        }
                                     </td>
                                     <td>
                                         <button onClick={() => handleDeleteUser(user)} className="btn btn-ghost btn-lg">
