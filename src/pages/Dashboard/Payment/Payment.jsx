fd7629e0c80ba02996adb9fa1c3afa92fea9c7d4
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-14 13:50:34 +0600

    created payment route, but not integrated

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle
+                subHeading={'Please Pay to Eat!'} heading={'payment'}
+            ></SectionTitle>
+        </div>
+    );
+};
+
+export default Payment;
