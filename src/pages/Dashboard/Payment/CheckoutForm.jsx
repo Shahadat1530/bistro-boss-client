eaa51d3fa7e9bdcbac3459c122f324009661ce6f
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-14 15:43:51 +0600

    created acc in stripe, and get the publish Key

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -17,6 +17,17 @@ const CheckoutForm = () => {
         if (card == null) {
             return;
         }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        if (error) {
+            console.log('payment error', error);
+        } else {
+            console.log('payment method', paymentMethod);
+        }
     }
 
 
@@ -38,7 +49,7 @@ const CheckoutForm = () => {
                     },
                 }}
             />
-            <button type="submit" disabled={!stripe}>
+            <button className='btn btn-sm btn-primary my-4' type="submit" disabled={!stripe}>
                 Pay
             </button>
         </form>
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 
 
-const stripePromise = loadStripe('');
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway);
 
 const Payment = () => {
     return (
@@ -14,7 +15,7 @@ const Payment = () => {
             ></SectionTitle>
             <div>
                 <Elements stripe={stripePromise}>
-
+                    <CheckoutForm></CheckoutForm>
                 </Elements>
             </div>
         </div>
